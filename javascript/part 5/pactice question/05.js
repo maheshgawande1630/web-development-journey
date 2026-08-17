f01725c5dbfd163225e0207f1e1b176c581439dd
Mahesh Gawande <maheshgawande681@gmail.com>
2026-08-17 23:15:13 +0530

    Solved all assignment question of part 5

---
+++ javascript/part 5/pactice question/05.js
@@ -0,0 +1,9 @@
+//Create an object representing a car that stores the following properties for the car: name, model, color. Print the car’s name
+
+const car={
+    name:"BMW",
+    model:"M4",
+    color:"Black"
+}
+
+console.log(car.name);
