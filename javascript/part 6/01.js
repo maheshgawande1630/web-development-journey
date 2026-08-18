9cc6d5e6d25c00c4d9f787b7d3bbf9d8cdd0f315
Mahesh Gawande <maheshgawande681@gmail.com>
2026-08-18 18:20:15 +0530

    function concept learned

---
+++ javascript/part 6/01.js
@@ -0,0 +1,13 @@
+//create a function to roll a dice and always display the value of the dice(1 to 6)
+
+function rollDice(){
+    let num=Math.floor(Math.random()*6)+1;
+    console.log(num);
+}
+
+rollDice();
+rollDice();
+rollDice();
+rollDice();
+rollDice();
+rollDice();
